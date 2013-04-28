f60b284bafdc4ea121c6902344ebe237af28652f
developer-kumamoto <developer.kumamoto@gmail.com>
2013-04-28 22:02:51 +0900

    #10-2

---
+++ tdd/src/Money/MoneyTest.java
@@ -0,0 +1,16 @@
+package Money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+
+}
