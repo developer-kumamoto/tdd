72fb3bb0d90a4eaa746b5e37685072851465b971
developer-kumamoto <developer.kumamoto@gmail.com>
2013-04-28 23:08:12 +0900

    #13

--- tdd/src/Money/MoneyTest.java
+++ tdd/src/Money/MoneyTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 public class MoneyTest {
 
 	@Test
-	public void test() {
+	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 
 }
